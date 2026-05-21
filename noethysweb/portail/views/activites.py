2723d7181b6aad91e14da0ed402f926d82de7cfb
Abderrahmane01Rhiabi <rhiabi.abderrahmane@gmail.com>
2026-05-21 10:50:02 +0200

    feat(portail/activites): support multi-famille pour compte individu
    - Vue: get_famille_object() + structure donnees_par_famille
    - Template: affichage par famille avec sections séparées
    - Chaque famille affiche ses propres activités et demandes

--- noethysweb/portail/views/activites.py
+++ noethysweb/portail/views/activites.py
@@ -8,11 +8,30 @@ logger = logging.getLogger(__name__)
 from django.db.models import Q
 from django.utils.translation import gettext as _
 from django.views.generic import TemplateView
-from core.models import Inscription, PortailRenseignement, Activite
+from core.models import Inscription, PortailRenseignement, Activite, Rattachement
 from portail.views.base import CustomView
 
 
-class View(CustomView, TemplateView):
+class Page(CustomView, TemplateView):
+
+    def get_famille_object(self):
+        """Retourne la/les familles rattachées à l'utilisateur."""
+        user = self.request.user
+        if hasattr(user, "famille") and user.famille:
+            return [user.famille]
+        if hasattr(user, "individu") and user.individu:
+            rattachements = Rattachement.objects.select_related("famille").filter(individu=user.individu, titulaire=1)
+            familles = []
+            seen_ids = set()
+            for rattachement in rattachements:
+                if rattachement.famille and rattachement.famille_id not in seen_ids:
+                    familles.append(rattachement.famille)
+                    seen_ids.add(rattachement.famille_id)
+            return familles
+        return []
+
+
+class View(Page):
     menu_code = "portail_activites"
     template_name = "portail/activites.html"
 
@@ -20,9 +39,62 @@ class View(CustomView, TemplateView):
         context = super(View, self).get_context_data(**kwargs)
         context['page_titre'] = _("Activités")
 
-        # Importation des inscriptions
-        conditions = Q(famille=self.request.user.famille) & Q(statut="ok") & (Q(date_fin__isnull=True) | Q(date_fin__gte=datetime.date.today())) & Q(individu__deces=False)
-        inscriptions = Inscription.objects.select_related("activite", "individu").filter(conditions).exclude(individu__in=self.request.user.famille.individus_masques.all())
+        familles = self.get_famille_object()
+        context["familles"] = familles
+
+        # -----------------------------
+        # Données par famille (multi-famille)
+        # -----------------------------
+        donnees_par_famille = []
+        dict_activites = {activite.pk: activite.nom for activite in Activite.objects.all()}
+        for famille in familles:
+            # Importation des inscriptions (par famille)
+            conditions = Q(famille=famille) & Q(statut="ok") & (Q(date_fin__isnull=True) | Q(date_fin__gte=datetime.date.today())) & Q(individu__deces=False)
+            inscriptions = Inscription.objects.select_related("activite", "individu").filter(conditions).exclude(individu__in=famille.individus_masques.all())
+
+            # Récupération des individus
+            liste_individus = sorted(list({inscription.individu for inscription in inscriptions}), key=lambda individu: individu.prenom)
+
+            # Récupération des activités pour chaque individu
+            dict_inscriptions = {}
+            for inscription in inscriptions:
+                dict_inscriptions.setdefault(inscription.individu, [])
+                dict_inscriptions[inscription.individu].append(inscription)
+
+            activites_par_individu = []
+            for individu in liste_individus:
+                activites_par_individu.append({
+                    "individu": individu,
+                    "inscriptions": sorted(dict_inscriptions.get(individu, []), key=lambda inscription: inscription.activite.nom),
+                })
+
+            # Demandes d'inscription en attente de traitement (par famille)
+            demandes = []
+            for demande in PortailRenseignement.objects.select_related("individu").filter(
+                famille=famille,
+                etat="ATTENTE",
+                code="inscrire_activite",
+            ).order_by("individu__prenom"):
+                demande.nom_activite = dict_activites.get(int(json.loads(demande.nouvelle_valeur).split(";")[0]), "?")
+                demandes.append(demande)
+
+            donnees_par_famille.append({
+                "famille": famille,
+                "activites": activites_par_individu,
+                "demandes_inscriptions_attente": demandes,
+            })
+
+        context["donnees_par_famille"] = donnees_par_famille
+
+        # -----------------------------
+        # Contexte historique (template actuel)
+        # -----------------------------
+        # Importation des inscriptions (toutes familles confondues)
+        conditions = Q(famille__in=familles) & Q(statut="ok") & (Q(date_fin__isnull=True) | Q(date_fin__gte=datetime.date.today())) & Q(individu__deces=False)
+        individus_masques = set()
+        for famille in familles:
+            individus_masques.update(list(famille.individus_masques.all()))
+        inscriptions = Inscription.objects.select_related("activite", "individu").filter(conditions).exclude(individu__in=individus_masques)
 
         # Récupération des individus
         context['liste_individus'] = sorted(list(set([inscription.individu for inscription in inscriptions])), key=lambda individu: individu.prenom)
@@ -37,8 +109,7 @@ class View(CustomView, TemplateView):
 
         # Demandes d'inscription en attente de traitement
         demandes = []
-        dict_activites = {activite.pk: activite.nom for activite in Activite.objects.all()}
-        for demande in PortailRenseignement.objects.select_related("individu").filter(famille=self.request.user.famille, etat="ATTENTE", code="inscrire_activite").order_by("individu__prenom"):
+        for demande in PortailRenseignement.objects.select_related("individu").filter(famille__in=familles, etat="ATTENTE", code="inscrire_activite").order_by("individu__prenom"):
             demande.nom_activite = dict_activites.get(int(json.loads(demande.nouvelle_valeur).split(";")[0]), "?")
             demandes.append(demande)
         context["demandes_inscriptions_attente"] = demandes
