21fb0e7d993e3999eaf14d1f3410dcdba484d053
Abderrahmane01Rhiabi <rhiabi.abderrahmane@gmail.com>
2026-05-22 15:36:50 +0200

    feat(portail/documents): support multi-famille et consentements
    - get_context_data() construit donnees_par_famille avec pieces_fournir,
      consentements et liste_pieces par famille
    - Template boucle sur donnees_par_famille avec titre par famille

--- noethysweb/portail/views/documents.py
+++ noethysweb/portail/views/documents.py
@@ -10,7 +10,7 @@ from django.views.generic import TemplateView
 from django.utils.translation import gettext as _
 from individus.utils import utils_pieces_manquantes
 from portail.utils import utils_approbations
-from core.models import PortailDocument
+from core.models import PortailDocument, Rattachement
 from core.utils import utils_dates
 
 
@@ -18,20 +18,74 @@ class View(CustomView, TemplateView):
     menu_code = "portail_documents"
     template_name = "portail/documents.html"
 
+    def get_famille_object(self):
+        """Retourne la/les familles rattachées à l'utilisateur."""
+        user = self.request.user
+        if hasattr(user, "famille") and user.famille:
+            return [user.famille]
+        if hasattr(user, "individu") and user.individu:
+            rattachements = Rattachement.objects.select_related("famille").filter(individu=user.individu, titulaire=1)
+            familles = []
+            seen_ids = set()
+            for rattachement in rattachements:
+                if rattachement.famille and rattachement.famille_id not in seen_ids:
+                    familles.append(rattachement.famille)
+                    seen_ids.add(rattachement.famille_id)
+            return familles
+        return []
+
     def get_context_data(self, **kwargs):
         context = super(View, self).get_context_data(**kwargs)
         context['page_titre'] = _("Documents")
 
-        # Pièces à fournir
-        context['pieces_fournir'] = utils_pieces_manquantes.Get_pieces_manquantes(famille=self.request.user.famille, exclure_individus=self.request.user.famille.individus_masques.all())
+        familles = self.get_famille_object()
+        context["familles"] = familles
 
-        # Importation des documents à télécharger
+        # Documents génériques (indépendants de la famille)
         liste_documents = []
         for document in PortailDocument.objects.all().order_by("titre"):
-            liste_documents.append({"titre": document.titre, "texte": document.texte, "fichier": document.document, "couleur_fond": document.couleur_fond, "extension": document.Get_extension()})
-        for unite_consentement in utils_approbations.Get_approbations_requises(famille=self.request.user.famille, avec_consentements_existants=False).get("consentements", []):
-            if unite_consentement.document.name:
-                liste_documents.append({"titre": unite_consentement.type_consentement.nom, "texte": "Version du %s" % utils_dates.ConvertDateToFR(unite_consentement.date_debut), "fichier": unite_consentement.document, "couleur_fond": "primary", "extension": unite_consentement.Get_extension()})
-        context['liste_documents'] = liste_documents
+            if document.document.name:
+                liste_documents.append({
+                    "titre": document.titre,
+                    "texte": document.texte,
+                    "fichier": document.document,
+                    "couleur_fond": document.couleur_fond,
+                    "extension": document.Get_extension(),
+            })
+        context["liste_documents"] = liste_documents
+
+        donnees_par_famille = []
+        for famille in familles:
+            data = {"famille": famille}
+
+            # Pièces à fournir
+            data["pieces_fournir"] = utils_pieces_manquantes.Get_pieces_manquantes(
+                famille=famille,
+                exclure_individus=famille.individus_masques.all(),
+            )
+
+            # Consentements (documents liés à la famille)
+            data["consentements"] = []
+            for unite_consentement in utils_approbations.Get_approbations_requises(
+                famille=famille,
+                avec_consentements_existants=False,
+            ).get("consentements", []):
+                if unite_consentement.document.name:
+                    data["consentements"].append({
+                        "titre": unite_consentement.type_consentement.nom,
+                        "texte": "Version du %s" % utils_dates.ConvertDateToFR(unite_consentement.date_debut),
+                        "fichier": unite_consentement.document,
+                        "couleur_fond": "primary",
+                        "extension": unite_consentement.Get_extension(),
+                    })
+
+            donnees_par_famille.append(data)
+
+        context["donnees_par_famille"] = donnees_par_famille
+        #print(f"DEBUG: donnees_par_famille = {donnees_par_famille}")
+
+        # Contexte historique (template précédent)
+        data_principale = donnees_par_famille[0] if donnees_par_famille else {}
+        context["pieces_fournir"] = data_principale.get("pieces_fournir", [])
 
         return context
