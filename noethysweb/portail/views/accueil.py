a98908f27c6873fef1be4410bf29003a43154e4c
Abderrahmane01Rhiabi <rhiabi.abderrahmane@gmail.com>
2026-05-29 10:04:04 +0200

    feat(portail/accueil): agréger les compteurs pour toutes les familles
    Pour un compte individu multi-famille, les compteurs (messages non lus,
    pièces manquantes, approbations, vaccinations, assurances, adhésions)
    agrégeaient seulement la première famille. Ils couvrent maintenant
    toutes les familles de l'utilisateur.

--- noethysweb/portail/views/accueil.py
+++ noethysweb/portail/views/accueil.py
@@ -23,11 +23,9 @@ class Accueil(CustomView, TemplateView):
         context = super(Accueil, self).get_context_data(**kwargs)
         context['page_titre'] = _("Accueil")
 
-        # Récupérer la famille (None si utilisateur individu)
-        famille = self.get_famille()
-        
-        # Si pas de famille (utilisateur individu sans famille), on retourne un contexte vide
-        if not famille:
+        familles = self.get_famille_object()
+
+        if not familles:
             context['nbre_informations_manquantes'] = 0
             context['nbre_pieces_manquantes'] = 0
             context['nbre_messages_non_lus'] = 0
@@ -37,50 +35,70 @@ class Accueil(CustomView, TemplateView):
             context['articles'] = []
             context['articles_popups'] = []
             return context
-        
+
         # Informations manquantes
-        context['nbre_informations_manquantes'] = utils_champs.Get_renseignements_manquants(famille=famille)["NBRE"]
+        context['nbre_informations_manquantes'] = sum(
+            utils_champs.Get_renseignements_manquants(famille=f)["NBRE"] for f in familles
+        )
 
         # Pièces manquantes
-        context['nbre_pieces_manquantes'] = len(utils_pieces_manquantes.Get_pieces_manquantes(famille=famille, only_invalides=True, exclure_individus=famille.individus_masques.all()))
+        context['nbre_pieces_manquantes'] = sum(
+            len(utils_pieces_manquantes.Get_pieces_manquantes(famille=f, only_invalides=True, exclure_individus=f.individus_masques.all()))
+            for f in familles
+        )
 
         # Messages non lus
-        context['nbre_messages_non_lus'] = len(PortailMessage.objects.filter(famille=famille, utilisateur__isnull=False, date_lecture__isnull=True))
+        context['nbre_messages_non_lus'] = PortailMessage.objects.filter(
+            famille__in=familles, utilisateur__isnull=False, date_lecture__isnull=True
+        ).count()
 
         # Approbations
-        approbations_requises = utils_approbations.Get_approbations_requises(famille=famille)
-        context['nbre_approbations_requises'] = approbations_requises["nbre_total"]
+        context['nbre_approbations_requises'] = sum(
+            utils_approbations.Get_approbations_requises(famille=f)["nbre_total"] for f in familles
+        )
 
-        # Récupération des activités de la famille
-        conditions = Q(famille=famille) & (Q(date_fin__isnull=True) | Q(date_fin__gte=datetime.date.today()))
+        # Inscriptions de toutes les familles
+        conditions = Q(famille__in=familles) & (Q(date_fin__isnull=True) | Q(date_fin__gte=datetime.date.today()))
         inscriptions = Inscription.objects.select_related("activite", "individu").filter(conditions)
         activites = list({inscription.activite: True for inscription in inscriptions}.keys())
 
         # Vaccins manquants
-        context["nbre_vaccinations_manquantes"] = sum([len(liste_vaccinations) for (famille, individu), liste_vaccinations in utils_vaccinations.Get_vaccins_obligatoires_by_inscriptions(inscriptions=inscriptions).items()])
+        context["nbre_vaccinations_manquantes"] = sum([
+            len(liste_vaccinations)
+            for (f, individu), liste_vaccinations in utils_vaccinations.Get_vaccins_obligatoires_by_inscriptions(inscriptions=inscriptions).items()
+        ])
 
         # Assurances manquantes
-        context["nbre_assurances_manquantes"] = len(utils_assurances.Get_assurances_manquantes_by_inscriptions(famille=famille, inscriptions=inscriptions))
+        context["nbre_assurances_manquantes"] = sum(
+            len(utils_assurances.Get_assurances_manquantes_by_inscriptions(famille=f, inscriptions=inscriptions.filter(famille=f)))
+            for f in familles
+        )
 
         # Adhésions manquantes
         if context["parametres_portail"].get("cotisations_afficher_page", False):
-            context["cotisations_manquantes"] = utils_cotisations_manquantes.Get_cotisations_manquantes(famille=famille, exclure_individus=famille.individus_masques.all())
+            cotisations_manquantes = []
+            for f in familles:
+                cotisations_manquantes += utils_cotisations_manquantes.Get_cotisations_manquantes(famille=f, exclure_individus=f.individus_masques.all())
+            context["cotisations_manquantes"] = cotisations_manquantes
 
-        # Articles
+        # Articles (basés sur les activités de toutes les familles)
         conditions = Q(statut="publie") & Q(date_debut__lte=datetime.datetime.now()) & (Q(date_fin__isnull=True) | Q(date_fin__gte=datetime.datetime.now()))
         conditions &= (Q(public__in=("toutes", "presents", "presents_groupes")) | (Q(public="inscrits") & Q(activites__in=activites)))
         articles = Article.objects.select_related("image_article", "album", "sondage", "auteur").filter(conditions).distinct().order_by("-date_debut")
         selection_articles = []
         popups = []
         for article in articles:
-            # Filtre les présents si besoin
             if article.public in ("presents", "presents_groupes"):
-                conditions = Q(inscription__famille=famille, date__gte=article.present_debut, date__lte=article.present_fin, etat__in=("reservation", "present"))
-                if article.public == "presents":
-                    conditions &= Q(activite__in=article.activites.all())
-                if article.public == "presents_groupes":
-                    conditions &= Q(groupe__in=article.groupes.all())
-                valide = Consommation.objects.filter(conditions).exists()
+                valide = False
+                for f in familles:
+                    cond = Q(inscription__famille=f, date__gte=article.present_debut, date__lte=article.present_fin, etat__in=("reservation", "present"))
+                    if article.public == "presents":
+                        cond &= Q(activite__in=article.activites.all())
+                    if article.public == "presents_groupes":
+                        cond &= Q(groupe__in=article.groupes.all())
+                    if Consommation.objects.filter(cond).exists():
+                        valide = True
+                        break
             else:
                 valide = True
             if valide:
@@ -89,13 +107,14 @@ class Accueil(CustomView, TemplateView):
                     popups.append(article)
         context['articles'] = selection_articles
 
-        # Popups
+        # Popups (on utilise la famille principale pour éviter les doublons)
+        famille_principale = familles[0]
         context['articles_popups'] = []
         if popups:
-            popups_lus = [lecture.article for lecture in Lecture.objects.filter(article__in=popups, famille=famille)]
+            popups_lus = [lecture.article for lecture in Lecture.objects.filter(article__in=popups, famille=famille_principale)]
             for popup in popups:
                 if popup not in popups_lus:
                     context['articles_popups'].append(popup)
-                    Lecture.objects.create(famille=famille, article=popup)
+                    Lecture.objects.create(famille=famille_principale, article=popup)
 
         return context
